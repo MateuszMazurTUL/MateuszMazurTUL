49bc1e218058f5a9b5e7da9f4d19ccd3c53066ca
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2021-11-07 20:27:38 +0100

    beta

--- PDS-WWW/kolofortuny/fortuna.js
+++ PDS-WWW/kolofortuny/fortuna.js
@@ -2,6 +2,9 @@ document.getElementById("btn_play").addEventListener("click", play);
 
 document.getElementById("btn_author").addEventListener("click", toggleAbout); 
 
+var country;
+var attempts = 6;
+
 var isAbout = false;
 function toggleAbout(){
     if (isAbout) {
@@ -13,9 +16,19 @@ function toggleAbout(){
     }
 }
 
+function hideAbout(){
+    isAbout = false;
+    document.getElementById("about").style.visibility = 'hidden';
+}
+
 function play(){
     refillAttempt();
     emptyMysteryWord();
+    hideBtn();//hide buttons
+    hideAbout();//hide about
+    showHelper();//show information about inut letter
+    newCountry();
+    activeKeyBind();
 }
 
 function refillAttempt(){
@@ -26,6 +39,8 @@ function refillAttempt(){
         lb[i].classList.remove("isLifeFalse");
         lb[i].classList.add('isLifeTrue');
     }
+    
+    attempts=6;
 }
 
 function removeAttempt(){
@@ -36,6 +51,8 @@ function removeAttempt(){
         if(lb[i].classList[2] == 'isLifeTrue') {
             lb[i].classList.remove("isLifeTrue");
             lb[i].classList.add('isLifeFalse');
+            attempts--;
+            checkAttemptsCondition();
             break;
         }
     }    
@@ -43,13 +60,78 @@ function removeAttempt(){
 
 function emptyMysteryWord(){
     var lb = new Array;
-    lb = document.getElementById("mystery_word").getElementsByClassName("underline");
+    lb = document.getElementById("mystery_word").innerHTML = '';
+}
+
+function prepareMysteryWord(length){
+    console.log(length);
+    for(i=0;i<length;i++) {
+        document.getElementById("mystery_word").innerHTML += '                <div class="underline"><span></span></div>';
+    }
+}
+
+function hideBtn(){
+    document.getElementById("btn_play").style.visibility = 'hidden';
+    document.getElementById("btn_author").style.visibility = 'hidden';
+}
+
+function showHelper(){
     
-    console.log(lb);
-    /*for(let i=0;i<=5;i++){
-        lb[i].classList.remove("isLifeFalse");
-        lb[i].classList.add('isLifeTrue');
-    }*/
+    document.getElementById("helper").style.visibility = 'visible';
+}
+
+function newCountry(){
+    let r = getRandomInt(0,data.length-1);
+    country = data[r]['country'];
+    console.log(country);
+    
+    prepareMysteryWord(country.length);
+}
+
+function checkAnswer(letter){
+    for(let i=0;i<country.length;i++){
+        
+        if (country[0].toLowerCase()==letter) return true;
+        if (country[i]==letter) return true;
+    };
+    return false;
+}
+
+function displayGoodLetter(letter){
+    for(let i=0;i<country.length;i++){
+        
+        if (country[0].toLowerCase()==letter) diplayLetter(letter.toUpperCase(),0);
+        if (country[i]==letter) diplayLetter(letter,i);
+    };
+}
+
+function diplayLetter(letter,pos){
+    let a= document.getElementById("mystery_word").getElementsByClassName("underline");
+    a[pos].innerHTML ="<span>"+letter+"</span>"
+    //console.log(a[pos]);
+            //<div class="underline"><span></span></div>';
+}
+
+function activeKeyBind(){
+    document.addEventListener("keydown",  function (e) {
+        var isGoodAnswer = checkAnswer(e.key);
+        if (isGoodAnswer) {
+            displayGoodLetter(e.key);
+            document.getElementById("letterKey").innerHTML = '<b class="goodAnswer">'+e.key+'</b>';
+        }
+        else{
+            removeAttempt();
+            document.getElementById("letterKey").innerHTML = '<b class="badAnswer">'+e.key+'</b>';
+        }
+
+//        console.log(e.keyCode);
+//        console.log(e.key);
+    }, true);
+}
+
+//check player lose
+function checkAttemptsCondition(){
+    if(attempts == 0) console.log("GAME OVER");
 }
 
 
@@ -58,57 +140,6 @@ function emptyMysteryWord(){
 
 
 
-
-
-
-
-
-var game = {
-  zdobyte : 0,
-  zycia : 1,
-}
-//alert(data[0]['country']);
-//var elem = document.getElementById("panstwa");
-//elem.innerHTML =data[0]['country'];
-
-//alert(data.length);
-//alert(data[0]['country'][2]);
-
- for (var i = 0; i < data[0]['country'].length; i += 1) {
-    //alert(data[0]['country'][i]);  
-  }
-
-
-addElement("wrap");
-//LISTENERS
-
-document.getElementById("graj").addEventListener("click", Sprawdz_Litery); 
-//alert(game.zycia);
-
-
-//FUNKCJE
-function Sprawdz_Litery(){
-  var liter = document.getElementById("wpisz_litere").value;
-  //alert(liter);
-  //alert(getRandomInt(10,20));
-}
-
-
-function addElement(mydiv)
-{
- 
-  newDiv = document.createElement("span");
-  newDiv.innerHTML = "";
-
-  my_div = document.getElementById(mydiv);
-  document.body.insertBefore(newDiv, my_div);
-
-  newDiv2 = document.createElement("span");
-  newDiv2.innerHTML = "jasiokotek2";
-  document.body.insertBefore(newDiv2, my_div.nextSibling);
-
-  newDiv.classList.add("mystyle");  
-}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
