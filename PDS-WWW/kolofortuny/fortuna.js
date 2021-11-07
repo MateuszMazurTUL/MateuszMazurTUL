8839d1d485fc526bc2a2a689c65863d651c11b44
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2021-11-07 18:56:30 +0100

    attempts bar and buttons

--- PDS-WWW/kolofortuny/fortuna.js
+++ PDS-WWW/kolofortuny/fortuna.js
@@ -1,3 +1,67 @@
+document.getElementById("btn_play").addEventListener("click", play); 
+
+document.getElementById("btn_author").addEventListener("click", toggleAbout); 
+
+var isAbout = false;
+function toggleAbout(){
+    if (isAbout) {
+        isAbout = !isAbout;
+        document.getElementById("about").style.visibility = 'hidden';
+    } else {
+        isAbout = !isAbout;
+        document.getElementById("about").style.visibility = 'visible';
+    }
+}
+
+function play(){
+    refillAttempt();
+    emptyMysteryWord();
+}
+
+function refillAttempt(){
+    var lb = new Array;
+    lb = document.getElementsByClassName("lifebar");
+    
+    for(let i=0;i<=5;i++){
+        lb[i].classList.remove("isLifeFalse");
+        lb[i].classList.add('isLifeTrue');
+    }
+}
+
+function removeAttempt(){
+    var lb = new Array;
+    lb = document.getElementsByClassName("lifebar");
+    
+    for(let i=5;i>=0;i--){
+        if(lb[i].classList[2] == 'isLifeTrue') {
+            lb[i].classList.remove("isLifeTrue");
+            lb[i].classList.add('isLifeFalse');
+            break;
+        }
+    }    
+}
+
+function emptyMysteryWord(){
+    var lb = new Array;
+    lb = document.getElementById("mystery_word").getElementsByClassName("underline");
+    
+    console.log(lb);
+    /*for(let i=0;i<=5;i++){
+        lb[i].classList.remove("isLifeFalse");
+        lb[i].classList.add('isLifeTrue');
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
 
 var game = {
   zdobyte : 0,
@@ -34,7 +98,7 @@ function addElement(mydiv)
 {
  
   newDiv = document.createElement("span");
-  newDiv.innerHTML = "hello";
+  newDiv.innerHTML = "";
 
   my_div = document.getElementById(mydiv);
   document.body.insertBefore(newDiv, my_div);
